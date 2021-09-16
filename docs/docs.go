4545fe69feb8e4c19a4378aa83e0e3d14256d209
Oleg <dmitrievoleg96@gmail.com>
2021-09-16 17:31:25 +0300

    swagger file added

---
+++ docs/docs.go
@@ -0,0 +1,29 @@
+// Package classification awesome.
+//
+// Documentation of our awesome API.
+//
+//     Schemes: http
+//     BasePath: /
+//     Version: 1.0.0
+//     Host: some-url.com
+//
+//     Consumes:
+//     - application/json
+//
+//     Produces:
+//     - application/json
+//
+//     Security:
+//     - basic
+//
+//    SecurityDefinitions:
+//    basic:
+//      type: basic
+//
+// swagger:meta
+package docs
+
+// swagger:route POST /foobar foobar-tag idOfFoobarEndpoint
+// Foobar does some amazing stuff.
+// responses:
+//   200: foobarResponse
